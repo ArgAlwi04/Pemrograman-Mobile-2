b43887a4e72ba3da90028e37aabe5db2fd16a790
ArgAlwi04 <argaalwi14@gmail.com>
2025-04-23 20:17:07 +0700

    Add files via upload

---
+++ main_menu.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'profile_screen.dart';
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [const LoginScreen(), const ProfileScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
